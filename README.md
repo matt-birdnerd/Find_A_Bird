8376b813e7d97762a980e51b7469afe23bd10660
Matt Allan <matt.birdnerd@gmail.com>
2024-01-27 19:02:45 -0600

    updated readme

--- README.md
+++ README.md
@@ -3,16 +3,70 @@
 ## Setup Instructions
 
 ### Database setup
-Install Postgresql
+Install Postgresql-16
+
+By default apt installs postgresql 14. We want 16 so we'll need to add the official postgres repository to our apt.
+Update our apt first
+        
     sudo apt update
-    sudo apt install postgresql postgresql-contrib
+
+Add the Postgres security keys to our trusted keys list
+
+    wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+
+Add the postgres repository while referencing our security keys
+
+    echo "deb [signed-by=/etc/apt/trusted.gpg.d/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+
+Update your repositories list
+
+    sudo apt-get update
+
+Install posgresql-16
+
+    sudo apt-get install postgresql-16
+
+Ensure it starts the service on boot
+
+    sudo systemctl start postgresql@16-main
+    sudo systemctl enable postgresql@16-main
+
+    
+Install PostGIS for latitude and longitude geometry queries
+
+    sudo apt-get install postgis postgresql-contrib
+
 
 Log into postgresql
 
     sudo -u postgres psql
 
+
 Create a user for the scripts to use
 
     CREATE USER find_a_bird WITH PASSWORD 'secure_password_goes_here';
+
+Give permissions to the script user
+
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO find_a_bird;
+
+Create the database
+
+    CREATE DATABASE findabird;
+
+Switch to your database
+
+    \c findabird
+
+Enable PostGIS extension
+
+    CREATE EXTENSION postgis;
+    CREATE EXTENSION postgis_topology;
+
+Verify PostGIS installation with:
+
+    SELECT PostGIS_full_version();
+
+Use the ```findabird_db_tables.sql``` file to initialize the database tables and columns.
 
 
---
+++ findabird_db_tables.sql
@@ -0,0 +1,133 @@
+CREATE TABLE IF NOT EXISTS "hotspots" (
+  "id" serial PRIMARY KEY,
+  "ebird_id" varchar(50),
+  "country_id" integer,
+  "subnational1_id" integer,
+  "subnational2_id" integer,
+  "latitude" decimal(9,6),
+  "longitude" decimal(9,6),
+  "geometry" "eometry(point, 4326),
+  "display_name" varchar(255),
+  "location_private" bool,
+  "create_date" timestamp
+);
+
+CREATE TABLE IF NOT EXISTS "countries" (
+  "id" serial PRIMARY KEY,
+  "country_code" varchar(2),
+  "display_name" varchar(100)
+);
+
+CREATE TABLE IF NOT EXISTS "subnational1" (
+  "id" serial PRIMARY KEY,
+  "subnational1_code" varchar(10),
+  "display_name" varchar(100),
+  "country_id" integer
+);
+
+CREATE TABLE IF NOT EXISTS "subnational2" (
+  "id" serial PRIMARY KEY,
+  "subnational2_code" varchar(10),
+  "display_name" varchar(100),
+  "country_id" integer,
+  "subnational1_id" integer
+);
+
+CREATE TABLE IF NOT EXISTS "users" (
+  "id" serial PRIMARY KEY,
+  "ebird_observer_id" varchar(50),
+  "first_name" varchar(50),
+  "last_name" varchar(50),
+  "email" varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS "species" (
+  "id" serial PRIMARY KEY,
+  "taxonomic_order" integer,
+  "common_name" varchar(255),
+  "sci_name" varchar(255),
+  "order" varchar(100),
+  "family" varchar(100),
+  "category" varchar(10),
+  "avibase_id" varchar(20),
+  "species_code" varchar(10),
+  "alpha_code_four" varchar(4),
+  "alpha_code_six" varchar(6)
+);
+
+CREATE TABLE IF NOT EXISTS "observations" (
+  "id" serial PRIMARY KEY,
+  "ebird_obs_id" varchar(50),
+  "checklist_id" integer,
+  "species_id" integer,
+  "count" integer,
+  "observation_reviewed" bool
+);
+
+CREATE TABLE IF NOT EXISTS "checklists" (
+  "id" serial PRIMARY KEY,
+  "ebird_id" varchar(50),
+  "user_id" integer,
+  "hotspot_id" integer,
+  "create_date" timestamp
+);
+
+CREATE TABLE IF NOT EXISTS "user_locations" (
+  "id" serial PRIMARY KEY,
+  "user_id" integer,
+  "polygon" GEOMETRY(POLYGON, 4326),
+  "location_name" varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS "species_needs" (
+  "id" serial PRIMARY KEY,
+  "user_id" integer,
+  "species_id" integer
+);
+
+CREATE TABLE IF NOT EXISTS "match_log" (
+  "id" serial PRIMARY KEY,
+  "user_id" integer,
+  "species_id" integer,
+  "observation_id" integer,
+  "user_location" integer,
+  "match_date" timestamp
+);
+
+COMMENT ON COLUMN "subnational1"."subnational1_code" IS 'State in the US';
+
+COMMENT ON COLUMN "subnational2"."subnational2_code" IS 'County/Parish in the US';
+
+ALTER TABLE "hotspots" ADD FOREIGN KEY ("country_id") REFERENCES "countries" ("id");
+
+ALTER TABLE "hotspots" ADD FOREIGN KEY ("subnational1_id") REFERENCES "subnational1" ("id");
+
+ALTER TABLE "hotspots" ADD FOREIGN KEY ("subnational2_id") REFERENCES "subnational2" ("id");
+
+ALTER TABLE "subnational1" ADD FOREIGN KEY ("country_id") REFERENCES "countries" ("id");
+
+ALTER TABLE "subnational2" ADD FOREIGN KEY ("country_id") REFERENCES "countries" ("id");
+
+ALTER TABLE "subnational2" ADD FOREIGN KEY ("subnational1_id") REFERENCES "subnational1" ("id");
+
+ALTER TABLE "observations" ADD FOREIGN KEY ("checklist_id") REFERENCES "checklists" ("id");
+
+ALTER TABLE "observations" ADD FOREIGN KEY ("species_id") REFERENCES "species" ("id");
+
+ALTER TABLE "checklists" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "checklists" ADD FOREIGN KEY ("hotspot_id") REFERENCES "hotspots" ("id");
+
+ALTER TABLE "user_locations" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "species_needs" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "species_needs" ADD FOREIGN KEY ("species_id") REFERENCES "species" ("id");
+
+ALTER TABLE "match_log" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "match_log" ADD FOREIGN KEY ("species_id") REFERENCES "species" ("id");
+
+ALTER TABLE "match_log" ADD FOREIGN KEY ("observation_id") REFERENCES "observations" ("id");
+
+ALTER TABLE "match_log" ADD FOREIGN KEY ("user_location") REFERENCES "user_locations" ("id");
